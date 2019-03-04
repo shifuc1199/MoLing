01768211a85d13f1ad17b7d715525ac47d4b5c53
shifuc1199 <745490282@qq.com>
2019-03-04 17:21:40 +0800

    清除无用素材

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UIManager : MonoBehaviour
+{
+    public Dropdown dropdown;
+    public GameObject[] BackGround;
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+
+    }
+    public void OnValueChange(int index)
+    {
+        if(index==0)
+        {
+            foreach (var item in BackGround)
+            {
+                item.SetActive(false);
+            }
+        }
+        else
+        {
+            foreach (var item in BackGround)
+            {
+                item.SetActive(true);
+             }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
